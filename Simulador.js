cfc347ed377eaac793d22c6f9aa4b04b3893586d
Jimmyforence <jimmyforence@gmail.com>
2022-09-06 04:24:18 -0500

    objetos y arrays listos

--- Simulador.js
+++ Simulador.js
@@ -2,24 +2,58 @@
 
 
 
-
 /*let btn = document.getElementsByClassName('btn');*/
 
-let contador=0
+let contador=1
+let contador2=1
+const playera1 = {Sku: 1001, precio: 800, Talla: "Extra Chica" }
+const playera2 = {Sku: 1002,precio: 800,Talla: "Chica" }
+const prodDisp =[playera1,playera2];
+
+document.querySelector(".btn").addEventListener("click", addCarrito, true);
+document.querySelector(".btnB").addEventListener("click", addCarrito2, true);
 
 
-document.querySelector(".btn").addEventListener("click", carrito, true);
 
-
-
-function carrito(){
+function addCarrito(){
     let limite = prompt("ingrese la cantidad de playeras que quieras");
     while ( contador <= limite) {
-        console.log(`la cantidad en carrito es ${contador}`);
+        // console.log(`la cantidad en carrito es ${contador}`)
+        // carrito.pop(playera1)
+        
+        
+        
+        console.log(`${contador} Producto(s)  agregado al carrito}`)
+        console.log(`costo del carrito es ${(playera2.precio*(contador2-1) )+(playera1.precio*(contador))}`)
+        console.log (carrito)
         contador++;
-        console.log(`Producto agregado`)
+        carrito.push(playera1)
+        costo.push(playera1.precio);
+    }
+    
+}
+
+
+function addCarrito2(){
+    let limite = prompt("ingrese la cantidad de playeras que quieras");
+    while ( contador2 <= limite) {
+        console.log(`la cantidad en carrito es ${contador2}`)
+        // carrito.pop(playera2)
+        
+        
+        
+        console.log(`${contador2} Producto(s)  agregado al carrito}`)
+        console.log(`costo del carrito es ${(playera2.precio*(contador2) )+(playera1.precio*(contador-1))}`)
+        console.log (carrito)
+        contador2++;
+        carrito.push(playera2)
+        costo.push(playera2.precio);
     }
 }
+
+
+const carrito = [];
+const costo = []
    
     /*if ( contador == 0) {
         contador= contador+1
